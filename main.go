cfbca964a34185ffc114b816ddd994885f7c74ac
slade <slade66@tutamail.com>
2025-11-08 18:04:33 +0800

    Storage 的结构体定义，Save 和 Load 方法的实现

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-
 func main() {
 	var todos Todos
-	todos.add("吃一个榴莲大福")
-	todos.add("喝一口豆浆")
+	// todos.add("吃一个榴莲大福")
+	// todos.add("喝一口豆浆")
+	
+	storage := NewStorage("todos.json")
+	storage.Load(&todos)
+	
 	todos.print()
+	
 	todos.delete(0)
 	todos.edit(0, "喝完豆浆")
 	todos.toggle(0)
+
 	todos.print()
+	storage.Save(todos)
 }
---
+++ storage.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Storage struct {
+	FileName string
+}
+
+func NewStorage(fileName string) *Storage {
+	return &Storage{
+		FileName: fileName,
+	}
+}
+
+func (storage *Storage) Save(data Todos) error {
+	jsonData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal Todos data: %w", err)
+	}
+	os.WriteFile(storage.FileName, jsonData, 0644)
+	return nil
+}
+
+func (storage *Storage) Load(todos *Todos) error {
+	jsonData, err := os.ReadFile(storage.FileName)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %w", err)
+	}
+	
+	if err := json.Unmarshal(jsonData, todos); err != nil {
+		return fmt.Errorf("failed to unmarshal Todos data: %w", err)
+	}
+	return nil
+}
